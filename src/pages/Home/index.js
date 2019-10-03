6b104fb3addf0b77a41b42c87f3f1e205ed13bce
Arieviloo <jadieoliveira00@gmail.com>
2019-10-03 18:03:47 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,113 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './style';
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      {/* cortar aqui */}
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="tenis"
+        />
+        <strong>Tenis dos bons</strong>
+        <span>R$ 190,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
