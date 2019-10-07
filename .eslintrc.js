9f01e8a7fdc417170d57cc787ca8c5f4bdef63c9
Arieviloo <jadieoliveira00@gmail.com>
2019-10-07 09:04:30 -0300

    Immer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   parser: 'babel-eslint',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off'
-  }
+    'react/state-in-constructor': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './config/ReactotronConfig';
 import Routes from './routes';
 import GlobalStyled from './styled/GlobalStyled';
 import Header from './Components/Header/index';
