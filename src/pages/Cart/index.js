cc12cb16dcd32c046510f1961d0a1f1a80933569
Arieviloo <jadieoliveira00@gmail.com>
2019-10-08 16:07:53 -0300

    Estoque no carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,12 +11,12 @@ import * as CartActions from '../../store/modules/cart/actions';
 import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './style';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function incrememt(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   return (
